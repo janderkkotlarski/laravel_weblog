2ca91e728c629b33fab8cd70abde7660292ce24f
janderkkotlarski <j.d.kotlarski@gmx.com>
2025-04-17 15:56:51 +0200

    migrations, factories and seeders work

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Article;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'article_id' => Article::inRandomOrder()->first()->id,
+            'entry' => $this->faker->sentence,
+        ];
+    }
+}
